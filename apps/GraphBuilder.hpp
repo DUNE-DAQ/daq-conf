22e32b4a6a8d1f13996025dccfef4e2c3ab96c0a
John Christian Freeman <jofreema@np04-srv-019.cern.ch>
2024-11-14 18:49:16 +0100

    JCF: DUNE-DAQ/daq-deliverables#149: clang format whitespace to prevent chaos when merging in develop

--- apps/GraphBuilder.hpp
+++ apps/GraphBuilder.hpp
@@ -29,8 +29,8 @@
 #include "confmodel/System.hpp"
 #include "ers/ers.hpp"
 
-#include "boost/graph/graph_traits.hpp"
 #include "boost/graph/adjacency_list.hpp"
+#include "boost/graph/graph_traits.hpp"
 #include "boost/graph/labeled_graph.hpp"
 
 #include <string>
@@ -39,126 +39,127 @@
 
 namespace daqconf {
 
-  class GraphBuilder {
+class GraphBuilder
+{
 
-  public:
+public:
+  using ConfigObject = dunedaq::conffwk::ConfigObject;
 
-    using ConfigObject = dunedaq::conffwk::ConfigObject;
+  struct VertexLabel;
+  struct EdgeLabel;
 
-    struct VertexLabel;
-    struct EdgeLabel;
+  // Switching container type to boost::listS seems to cause
+  // compilation problems with the boost::write_graphviz function...
+  using Graph_t = boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, VertexLabel, EdgeLabel>;
+  using Edge_t = boost::graph_traits<Graph_t>::edge_descriptor;
+  using Vertex_t = boost::graph_traits<Graph_t>::vertex_descriptor;
 
-    // Switching container type to boost::listS seems to cause
-    // compilation problems with the boost::write_graphviz function...
-    using Graph_t = boost::adjacency_list<boost::vecS, boost::vecS, boost::bidirectionalS, VertexLabel, EdgeLabel>;
-    using Edge_t = boost::graph_traits<Graph_t>::edge_descriptor;
-    using Vertex_t = boost::graph_traits<Graph_t>::vertex_descriptor;
-
-    enum class ObjectKind {
-      kUndefined,
-      kSystem,
-      kSegment,
-      kApplication,
-      kModule,
-      kIncomingExternal,   // Object is meant to represent the outside world, not an actual component of the DAQ
-      kOutgoingExternal    // ""
-    };
-
-    struct VertexLabel {
-
-      VertexLabel() = default;
-      
-      VertexLabel(const std::string& uid, const std::string& classname) :
-	displaylabel(uid + "\n" + classname)
-      {}
-
-      const std::string displaylabel {"undefined"};
-    };
-
-    struct EdgeLabel {
-      const std::string displaylabel {"undefined"};
-    };
-
-    explicit GraphBuilder(const std::string& oksfilename, const std::string& systemname);
-
-    void construct_graph(std::string root_obj_uid);
-    void write_graph(const std::string& outputfilename) const;
-    
-    GraphBuilder(const GraphBuilder&) = delete;
-    GraphBuilder(GraphBuilder&&) = delete;
-    GraphBuilder& operator=(const GraphBuilder&) = delete;
-    GraphBuilder& operator=(GraphBuilder&&) = delete;
-
-  private:
-
-    struct EnhancedObject {
-
-      struct ReceivingInfo {
-	std::string connection_name;
-	std::string receiver_label;
-
-	bool operator==(const ReceivingInfo& other) const = default;
-      };
-
-      EnhancedObject(const ConfigObject& config_object_arg, ObjectKind kind_arg) :
-	config_object {config_object_arg},
-	kind {kind_arg}
-      {}
-
-      ConfigObject config_object;
-      ObjectKind kind;
-
-      Vertex_t vertex_in_graph;
-
-      // What objects is this one the parent of? E.g., a parent system with child segments
-      std::vector<std::string> child_object_names;
-
-      // What objects does this one send data to, and what are their connections called?
-      std::vector<ReceivingInfo> receiving_object_infos; 
-    };
-
-    void find_candidate_objects();
-    [[nodiscard]] std::vector<dunedaq::conffwk::ConfigObject> find_child_objects(const ConfigObject& parent_obj);
-    void calculate_graph(const std::string& root_obj_uid);
-
-    // find_objects_and_connections fills m_objects_for_graph as well
-    // as m_incoming_connections and m_outgoing_connections
-    
-    void find_objects_and_connections(const ConfigObject& object);
-    void calculate_network_connections();
-
-    const std::string m_oksfilename;
-    dunedaq::conffwk::Configuration* m_confdb;
-
-    const std::unordered_map<ObjectKind, std::vector<std::string>> m_included_classes;
-
-    std::unordered_map<std::string, EnhancedObject> m_objects_for_graph;
-
-    std::unordered_map<std::string, std::vector<std::string>> m_incoming_connections;
-    std::unordered_map<std::string, std::vector<std::string>> m_outgoing_connections;
-    
-    Graph_t m_graph;
-    ObjectKind m_root_object_kind;
-
-    dunedaq::confmodel::System* m_system;
-    std::string m_system_name;
-    
-    std::vector<std::string> m_ignored_application_uids;
-    
-    std::vector<ConfigObject> m_all_objects;
-    std::vector<ConfigObject> m_candidate_objects;
+  enum class ObjectKind
+  {
+    kUndefined,
+    kSystem,
+    kSegment,
+    kApplication,
+    kModule,
+    kIncomingExternal, // Object is meant to represent the outside world, not an actual component of the DAQ
+    kOutgoingExternal  // ""
   };
 
-  [[nodiscard]] constexpr GraphBuilder::ObjectKind get_object_kind(const std::string& class_name);
+  struct VertexLabel
+  {
+
+    VertexLabel() = default;
+
+    VertexLabel(const std::string& uid, const std::string& classname)
+      : displaylabel(uid + "\n" + classname)
+    {
+    }
+
+    const std::string displaylabel{ "undefined" };
+  };
+
+  struct EdgeLabel
+  {
+    const std::string displaylabel{ "undefined" };
+  };
+
+  explicit GraphBuilder(const std::string& oksfilename, const std::string& systemname);
+
+  void construct_graph(std::string root_obj_uid);
+  void write_graph(const std::string& outputfilename) const;
+
+  GraphBuilder(const GraphBuilder&) = delete;
+  GraphBuilder(GraphBuilder&&) = delete;
+  GraphBuilder& operator=(const GraphBuilder&) = delete;
+  GraphBuilder& operator=(GraphBuilder&&) = delete;
+
+private:
+  struct EnhancedObject
+  {
+
+    struct ReceivingInfo
+    {
+      std::string connection_name;
+      std::string receiver_label;
+
+      bool operator==(const ReceivingInfo& other) const = default;
+    };
+
+    EnhancedObject(const ConfigObject& config_object_arg, ObjectKind kind_arg)
+      : config_object{ config_object_arg }
+      , kind{ kind_arg }
+    {
+    }
+
+    ConfigObject config_object;
+    ObjectKind kind;
+
+    Vertex_t vertex_in_graph;
+
+    // What objects is this one the parent of? E.g., a parent system with child segments
+    std::vector<std::string> child_object_names;
+
+    // What objects does this one send data to, and what are their connections called?
+    std::vector<ReceivingInfo> receiving_object_infos;
+  };
+
+  void find_candidate_objects();
+  [[nodiscard]] std::vector<dunedaq::conffwk::ConfigObject> find_child_objects(const ConfigObject& parent_obj);
+  void calculate_graph(const std::string& root_obj_uid);
+
+  // find_objects_and_connections fills m_objects_for_graph as well
+  // as m_incoming_connections and m_outgoing_connections
+
+  void find_objects_and_connections(const ConfigObject& object);
+  void calculate_network_connections();
+
+  const std::string m_oksfilename;
+  dunedaq::conffwk::Configuration* m_confdb;
+
+  const std::unordered_map<ObjectKind, std::vector<std::string>> m_included_classes;
+
+  std::unordered_map<std::string, EnhancedObject> m_objects_for_graph;
+
+  std::unordered_map<std::string, std::vector<std::string>> m_incoming_connections;
+  std::unordered_map<std::string, std::vector<std::string>> m_outgoing_connections;
+
+  Graph_t m_graph;
+  ObjectKind m_root_object_kind;
+
+  dunedaq::confmodel::System* m_system;
+  std::string m_system_name;
+
+  std::vector<std::string> m_ignored_application_uids;
+
+  std::vector<ConfigObject> m_all_objects;
+  std::vector<ConfigObject> m_candidate_objects;
+};
+
+[[nodiscard]] constexpr GraphBuilder::ObjectKind
+get_object_kind(const std::string& class_name);
 
 } // namespace daqconf
 
-
-ERS_DECLARE_ISSUE(daqconf,                 
-                  GeneralGraphToolError,
-                  "A graph tool error occured: " << errmsg,
-                  ((std::string)errmsg)
-)
-
+ERS_DECLARE_ISSUE(daqconf, GeneralGraphToolError, "A graph tool error occured: " << errmsg, ((std::string)errmsg))
 
 #endif // DAQCONF_APPS_GRAPHBUILDER_HPP_
--- apps/create_config_plot.cxx
+++ apps/create_config_plot.cxx
@@ -28,52 +28,51 @@
 
 namespace bpo = boost::program_options;
 
-int main(int argc, char* argv[])
+int
+main(int argc, char* argv[])
 {
 
-  std::string oksfilename {""};
-  std::string outputfilename {""};
-  std::string object_uid {""};
-  std::string systemname {""};
+  std::string oksfilename{ "" };
+  std::string outputfilename{ "" };
+  std::string object_uid{ "" };
+  std::string systemname{ "" };
 
-  bpo::options_description options_description (
-    "Allowed options", 128 );
+  bpo::options_description options_description("Allowed options", 128);
 
   options_description.add_options()
 
-  ( "help,h", "Provide help message" )
+    ("help,h", "Provide help message")
 
-  ( "file,f", bpo::value<std::string> ( &oksfilename ), "OKS database file name" )
+      ("file,f", bpo::value<std::string>(&oksfilename), "OKS database file name")
 
-    ( "root-object,r", bpo::value<std::string>(&object_uid)->default_value(""), "OKS object UID of root vertex; must be system, segment or application")
-    ( "system,s", bpo::value<std::string> ( &systemname )->default_value(""),
-    "Name of the system associated with the root object (only needed if >1 system in the database)" )
-    ( "output,o", bpo::value<std::string> ( &outputfilename )->default_value("config.dot"),
-    "Output DOT file which can be used as input to GraphViz" );
+        ("root-object,r",
+         bpo::value<std::string>(&object_uid)->default_value(""),
+         "OKS object UID of root vertex; must be system, segment or application")(
+          "system,s",
+          bpo::value<std::string>(&systemname)->default_value(""),
+          "Name of the system associated with the root object (only needed if >1 system in the database)")(
+          "output,o",
+          bpo::value<std::string>(&outputfilename)->default_value("config.dot"),
+          "Output DOT file which can be used as input to GraphViz");
 
   bpo::variables_map args;
 
-  auto display_help_message = [&options_description]()
-  {
-    TLOG() 
-        << "create_config_plot : Generate dot graphs from database files"
-        << std::endl
-        << std::endl
-        << "Usage: create_config_plot -f/--file <input OKS file> -r/--root-object <object UID for system, segment or application> (-s/--system <system containing root-object>) (-o/--output <output DOT file, default is config.dot>)"
-        << std::endl
-        << std::endl
-        << options_description
-        << std::endl;
+  auto display_help_message = [&options_description]() {
+    TLOG() << "create_config_plot : Generate dot graphs from database files" << std::endl
+           << std::endl
+           << "Usage: create_config_plot -f/--file <input OKS file> -r/--root-object <object UID for system, segment "
+              "or application> (-s/--system <system containing root-object>) (-o/--output <output DOT file, default is "
+              "config.dot>)"
+           << std::endl
+           << std::endl
+           << options_description << std::endl;
   };
 
-  try
-  {
-    bpo::store ( bpo::command_line_parser ( argc, argv ).options ( options_description ).run(),
-                 args );
-    bpo::notify ( args );
-    
-    if ( args.count ( "help" ) || ! args.count ( "file" ) )
-    {
+  try {
+    bpo::store(bpo::command_line_parser(argc, argv).options(options_description).run(), args);
+    bpo::notify(args);
+
+    if (args.count("help") || !args.count("file")) {
       display_help_message();
       return EXIT_FAILURE;
     }
@@ -81,11 +80,11 @@ int main(int argc, char* argv[])
     daqconf::GraphBuilder graphbuilder(oksfilename, systemname);
     graphbuilder.construct_graph(object_uid);
     graphbuilder.write_graph(outputfilename);
-    
+
   } catch (const bpo::error& e) {
 
     display_help_message();
-    
+
     std::stringstream errmsgstr;
     errmsgstr << "Incorrect command line argument: " << e.what();
     ers::fatal(daqconf::GeneralGraphToolError(ERS_HERE, errmsgstr.str()));
@@ -95,12 +94,10 @@ int main(int argc, char* argv[])
     errmsgstr << "Caught BadConf exception: " << exc;
     ers::fatal(daqconf::GeneralGraphToolError(ERS_HERE, errmsgstr.str()));
 
-  } catch(const daqconf::GeneralGraphToolError& e) {
+  } catch (const daqconf::GeneralGraphToolError& e) {
 
     ers::fatal(e);
-
   }
 
   return 0;
 }
-
